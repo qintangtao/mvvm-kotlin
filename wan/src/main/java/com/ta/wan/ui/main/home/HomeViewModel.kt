b097a8f3a6f39c662b51f2e788935c5ab9fed095
liyutang <515898099@qq.com>
2020-07-01 16:00:27 +0800

    StateLayout modify

--- wan/src/main/java/com/ta/wan/ui/main/home/HomeViewModel.kt
+++ wan/src/main/java/com/ta/wan/ui/main/home/HomeViewModel.kt
@@ -3,11 +3,22 @@ package com.ta.wan.ui.main.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.ta.mvvm.base.BaseViewModel
+import com.ta.mvvm.network.ResponseThrowable
+import kotlinx.coroutines.delay
+import java.lang.RuntimeException
 
 class HomeViewModel : BaseViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val text = MutableLiveData<String>()
+
+    fun testStatus(isNotify: Boolean = true): MutableLiveData<String> {
+        launchGo({
+            delay(2000)
+            //throw ResponseThrowable(3000, "error ")
+            text.value = ""
+            //This is home Fragment
+        }, isNotify = isNotify)
+        return text
     }
-    val text: LiveData<String> = _text
+
 }
